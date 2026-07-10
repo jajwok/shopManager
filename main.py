e4ccfa3ba34b04002a5f9b2c9ae84439d597c0e9
greg <gregjerry22@gmail.com>
2026-07-10 23:08:19 +0300

    ädded sqlite3

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from kivy.app import App
 from kivy.uix.label import Label    
+import sqlite3
+from name import name
+# class dataEntryForm(GridLayout):
+#   pass  
 
-class dataEntryForm(GridLayout):
-    
-    
+# database connection
+with sqlite3.connect('shop.db') as connection:
+    # Create a cursor object
+    cursor = connection.cursor()
+
+# Close the database connection
+connection.close()
 
 
-class MyApp(dataEntryForm):
+class MyApp(App):
     def build(self):
-        return dataEntryForm()
+        return Label(text=f"Hello  {name}")
     
 if __name__ == '__main__':
     MyApp().run()
